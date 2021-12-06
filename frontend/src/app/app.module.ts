c0fe77e70e7c8b7f29b30d1adcfd8aee8ab08d4e
Rohit Nakwal <rohit_nakwal@persistent.com>
2021-12-07 00:39:33 +0530

    Form Validation is added in signUp Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/signUp/signUp.component.ts
+++ frontend/src/app/components/signUp/signUp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signUp',
@@ -10,6 +11,10 @@ export class SignUpComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  signUpUser(signInForm: NgForm){
+    console.log(signInForm);
   }
 
 }
